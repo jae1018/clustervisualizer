c721376c5a58e1d200a98c0dd1ba35708a18956d
James Edmond <edmondandy795@gmail.com>
2023-01-24 13:22:17 -0500

    Init Files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 15:45:08 2021
+
+@author: jedmond
+"""
+
+from setuptools import setup, find_packages
+
+#### TO DO
+
+#setup(
+#    name='Unsupervised_THEMIS_Clustering_Project',
+#    version="1.0",
+#    packages=find_packages(),
+#)
