d37cb157eb761b39053259f7f0b97a3e019ff311
James Edmond <edmondandy795@gmail.com>
2023-01-25 11:41:55 -0500

    (Tried) to fix the subpackage import structure

--- clustervisualizer/ClusterAnalyzer/__init__.py
+++ clustervisualizer/ClusterAnalyzer/__init__.py
@@ -10,10 +10,10 @@ Created on Fri Jul 22 13:42:27 2022
 
 ##from ClusterAnalyzer import ClusterAnalyzer
 ##from ClusterAnalyzer.ClusterAnalyzer import ClusterAnalyzer
-from . import ClusterAnalyzer
+#####from . import ClusterAnalyzer
 #from SCDataProc.sc_constants import sc_constants
 #from SCDataProc.sc_defs import sc_defs
 #import SCDataProc.utils as utils
 ##import ClusterAnalyzer.utils as utils
-from . import utils
-__all__ = [ "ClusterAnalyzer", "utils"]
+#####from . import utils
+#####__all__ = [ "ClusterAnalyzer", "utils"]
