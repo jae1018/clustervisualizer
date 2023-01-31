ef2593caec163771ff6288953f055c90108c8a85
James Edmond <edmondandy795@gmail.com>
2023-01-31 14:44:14 -0500

    Finished hard_clustering_example notebook

--- clustervisualizer/examples/hard_clustering_example.py
+++ clustervisualizer/examples/hard_clustering_example.py
@@ -16,24 +16,18 @@ X, y = load_iris(as_frame=True, return_X_y=True)
 
 
 #### Cluster data using hard clustering
-kmeans_obj = KMeans(n_clusters=3)
+kmeans_obj = KMeans(n_clusters=3, random_state=1)
 preds = kmeans_obj.fit_predict(X)
 
 
 
 ####  Perform cluster analysis
+names = { 'setosa': ('petal width (cm)', lambda x: -1*np.mean(x)),
+          'virginica': 'petal length (cm)',
+          'versicolor': ('sepal width (cm)', lambda x: -1*np.mean(x)) }
 ## Make cluster analysis instance and name clusters
-clust_inst = clust(
-                X,
-                preds,
-                name_clusters={
-                    'setosa': ('petal width (cm)', 
-                               lambda x: -1*np.mean(x)),
-                    'virginica': 'petal length (cm)',
-                    'versicolor': ('sepal width (cm)',
-                                   lambda x: -1*np.mean(x))
-                                }
-                    )
+clust_inst = clust(X, preds,
+                   name_clusters = names)
 ## Create histograms
 clust_inst.hist1d(bins=15)
 clust_inst.hist2d(bins=(10,10))
