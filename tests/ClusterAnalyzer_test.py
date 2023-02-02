1cd5c8e31c905d756ad498482a8d84c3c844a2c6
James Edmond <edmondandy795@gmail.com>
2023-02-02 17:05:04 -0500

    forgot to incorporate new testing setup

--- tests/ClusterAnalyzer_test.py
+++ tests/ClusterAnalyzer_test.py
@@ -24,22 +24,79 @@ def test_ClusterAnalyzer_init():
 
 
 
-def test_ClusterAnalyzer_build_constraints_list():
+
+
+
+
+
+class Test_ClusterAnalyzer_build_constraints_list:
     
-    df = pd.DataFrame({
-        'cat': np.hstack(
-            [ np.full(10,elem) for elem in ['a','b','c'] ]
-                        )
-                      })
-    preds = np.full(df.shape[0], 0)
-    _clust = clust(df, preds)
+    #self._int_type = np.arange(1).dtype.type
+    _empty_int_arr = np.array([]).astype( np.arange(1).dtype.type )
     
-    algo_result = _clust._build_constraints_list_of_dicts('cat')
-    expected_result = [ {'cat':'a'},
-                        {'cat':'b'},
-                        {'cat':'c'} ]
-    assert expected_result == algo_result
     
+    
+    def compare_expected_vs_algo_result(self, algo, expec):
+        
+        # check length
+        assert len(algo) == len(expec)
+        
+        #try:
+        for i in range(len(algo)):
+            assert algo[i][0] == expec[i][0]
+            np.testing.assert_array_equal(algo[i][1], expec[i][1])
+            
+
+
+
+
+    def test_build_constraints_list_one_categ_multiple_values(self):
+        
+        df = pd.DataFrame({
+            'cat': np.hstack(
+                [ np.full(10,elem) for elem in ['a','b','c'] ]
+                            )
+                          })
+        preds = np.full(df.shape[0], 0)
+        _clust = clust(df, preds)
+        
+        algo_result = _clust._build_constraints_list_of_dicts('cat')
+        expected_result = [
+            ( {'cat':'a'}, np.arange(10) ),
+            ( {'cat':'b'}, np.arange(10)+10 ),
+            ( {'cat':'c'}, np.arange(10)+20 )
+                          ]
+        self.compare_expected_vs_algo_result(algo_result, expected_result)
+        #assert expected_result == algo_result
+
+
+
+
+
+    def test_build_constraints_list_multiple_categ_multiple_values(self):
+        
+        df = pd.DataFrame({
+            'X': np.hstack(
+                [ np.full(10,elem) for elem in ['a','b','c'] ]
+                            ),
+            'Y': np.hstack(
+                [ np.full(15,elem) for elem in ['1','2'] ]
+                            )
+                          })
+        preds = np.full(df.shape[0], 0)
+        _clust = clust(df, preds)
+        
+        algo_result = _clust._build_constraints_list_of_dicts(['X','Y'])
+        expected_result = [
+            ( {'X':'a', 'Y':'1'}, np.arange(10) ),
+            ( {'X':'a', 'Y':'2'}, self._empty_int_arr ),
+            ( {'X':'b', 'Y':'1'}, np.arange(5)+10 ),
+            ( {'X':'b', 'Y':'2'}, np.arange(5)+15 ),
+            ( {'X':'c', 'Y':'1'}, self._empty_int_arr ),
+            ( {'X':'c', 'Y':'2'}, np.arange(10)+20 )
+                          ]
+        self.compare_expected_vs_algo_result(algo_result, expected_result)
+        #assert expected_result == algo_result    
     
     
     
